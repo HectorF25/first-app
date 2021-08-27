edbaec4f072c1dadac792ce39c0d0f4ae4e40e12
Hector Florez <hectorflorez25@gmail.com>
2021-08-26 22:05:19 -0500

    release/HectorF25-add-route-info

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom'; 
 import Home from './views/home';
+import Info from './views/info';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <Router>
       <Route component={Home} path={'/home'} />
+      <Route component={Info} path={'/info'} />
     </Router>
   );
 }
---
+++ src/views/info.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import FooterComponent from '../components/footerComponent';
+import HeaderComponent from '../components/headerComponent';
+import '../css/home.css';
+
+function Info() {
+    return(
+    <div className="contendeor">
+        <HeaderComponent/>
+        <FooterComponent/>
+    </div>
+    )
+};
+export default Info;
